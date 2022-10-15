7a310fd1b54d5aa5c3626f9e6db51b59d2527f9a
SeraphJACK <seraphjack@outlook.com>
2022-10-15 19:22:42 +0800

    Save & delete buttons

--- src/components/Memo/index.tsx
+++ src/components/Memo/index.tsx
@@ -1,7 +1,9 @@
 import { getMemo } from "@/api/api";
 import { MemoDetail, MemoInfo } from "@/api/schema";
 import useMessage from "@/hooks/useMessage";
-import { Grid, Tab, Tabs, TextField } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import SaveIcon from "@mui/icons-material/Save";
+import { Grid, IconButton, Tab, Tabs, TextField, Tooltip } from "@mui/material";
 import React, { ChangeEvent, useCallback, useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import MonacoEditor from "react-monaco-editor";
@@ -10,11 +12,21 @@ import Loading from "../Loading";
 import "./index.css";
 
 interface Props {
-  memo: MemoInfo;
+  memo: MemoInfo | null;
+  showDelete?: boolean;
+  defaultTab?: "view" | "edit";
+  handleSave?: (memo: MemoDetail) => {};
+  handleDelete?: (memo: MemoDetail) => {};
 }
 
-const Memo: React.FunctionComponent<Props> = ({ memo }: Props) => {
-  const [tab, setTab] = useState<"view" | "edit">("view");
+const Memo: React.FunctionComponent<Props> = ({
+  memo,
+  showDelete = true,
+  defaultTab = "view",
+  handleSave,
+  handleDelete,
+}: Props) => {
+  const [tab, setTab] = useState<"view" | "edit">(defaultTab);
   const [loading, setLoading] = useState(true);
   const [detail, setDetail] = useState<MemoDetail | null>(null);
 
@@ -78,23 +90,63 @@ const Memo: React.FunctionComponent<Props> = ({ memo }: Props) => {
 
   useEffect(() => {
     (async () => {
+      setLoading(true);
+      setTab(defaultTab);
       try {
-        setDetail(await getMemo(memo.id));
+        if (memo) {
+          setDetail(await getMemo(memo.id));
+        } else {
+          setDetail({
+            id: -1,
+            title: "",
+            content: "",
+            lastModified: "",
+          });
+        }
       } catch (e) {
         addMessage("error", "加载备忘录失败");
       } finally {
         setLoading(false);
       }
     })();
-  }, [memo, setLoading, setDetail]);
+  }, [memo, defaultTab, setLoading, setTab, setDetail]);
 
   return (
     <Grid container justifyContent="center" className="memo-container">
       <Grid item xs={10}>
-        <Tabs value={tab} onChange={(_event, newVal) => setTab(newVal)}>
-          <Tab value="view" label="查看" />
-          <Tab value="edit" label="编辑" />
-        </Tabs>
+        <Grid container>
+          <Grid item xs={5}>
+            <Tabs value={tab} onChange={(_event, newVal) => setTab(newVal)}>
+              <Tab value="view" label="查看" />
+              <Tab value="edit" label="编辑" />
+            </Tabs>
+          </Grid>
+          <Grid item xs></Grid>
+          {detail !== null && tab === "edit" && (
+            <>
+              <Grid item xs={1}>
+                <Tooltip title="保存">
+                  <IconButton onClick={() => handleSave && handleSave(detail)}>
+                    <SaveIcon />
+                  </IconButton>
+                </Tooltip>
+              </Grid>
+              {showDelete ? (
+                <Grid item xs={1}>
+                  <Tooltip title="删除">
+                    <IconButton
+                      onClick={() => handleDelete && handleDelete(detail)}
+                    >
+                      <DeleteIcon />
+                    </IconButton>
+                  </Tooltip>
+                </Grid>
+              ) : (
+                <></>
+              )}
+            </>
+          )}
+        </Grid>
       </Grid>
       <Grid item xs={10} sx={{ marginTop: "20px" }}>
         {body}
