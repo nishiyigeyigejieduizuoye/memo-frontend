460901dc9380def30d88a6915de14737f14d159c
SeraphJACK <seraphjack@outlook.com>
2022-10-15 10:05:52 +0800

    Memo api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { UserInfo } from "./schema";
+import { MemoDetail, MemoInfo, UserInfo } from "./schema";
 
 const instance = axios.create({
   timeout: 10000,
@@ -25,4 +25,27 @@ export async function register(username: string, password: string) {
 
 export async function logout() {
   return await instance.post("/api/user/logout");
+}
+
+export async function listMemos(): Promise<MemoInfo[]> {
+  return (await instance.get<MemoInfo[]>("/api/memo/")).data;
+}
+
+export async function getMemo(id: number): Promise<MemoDetail> {
+  return (await instance.get<MemoDetail>("/api/memo/" + id.toString())).data;
+}
+
+export async function createMemo(title: string, content: string) {
+  return await instance.post("/api/memo/", { title, content });
+}
+
+export async function patchMemo(memo: MemoDetail) {
+  return await instance.patch("/api/memo/" + memo.id.toString(), {
+    title: memo.title,
+    content: memo.content,
+  });
+}
+
+export async function deleteMemo(id: number) {
+  return await instance.delete("/api/memo/" + id.toString());
 }
